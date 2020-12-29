e28ce35c85ab96a51621fbfa06ed239185d14d48
guillaume-plantevin <71876345+guillaume-plantevin@users.noreply.github.com>
2020-12-29 10:13:11 +0100

    Create discussion.php

---
+++ discussion.php
@@ -0,0 +1,11 @@
+<?php
+  /*
+    Une page contenant le fil de discussion (discussion.php) :
+    Sur cette page, les utilisateurs connectés peuvent voir 
+    l’ensemble des messages dans un fil de discussion. 
+    En dessous du fil de discussion se trouvent un champs contenant 
+    le message et un bouton permettant de l’envoyer. 
+    Les utilisateurs non connectés souhaitant accéder 
+    à cette page sont redirigés vers la page de connexion.
+  */
+?>
