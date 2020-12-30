ee3a7f0e9c3a823dab64aa54083bd78c1f5727a0
albertini <keany.albertini@laplateforme.io>
2020-12-30 15:33:21 +0100

    rajout session start et personnalisation des messages d'erreur, rajout du post redirect get indiqué par guigui la machine

--- inscription.php
+++ inscription.php
@@ -7,96 +7,99 @@
     les données sont insérées dans la base de données 
     et l’utilisateur est redirigé vers la page de connexion.
   */
-   
+
+    session_start();
     require_once('pdo.php');
-
-
-    $error = null;
-
-
-
 
 	if (isset($_POST['submit'])) 
 	{
-		echo 'inside submit<br>';
 		$length_login = strlen($_POST['login']);
     	$length_password = strlen($_POST['password']);
 
 		if ($_POST['password'] == $_POST['password_confirm'])
 		{
-			// DEBUG
-			echo "password OK<br>";
 			if ($length_login <= 255 AND $length_password <=255) 
 			{
-				// DEBUG
-				echo '30: length: OK<br>';
 				$count = $pdo->prepare("SELECT * FROM `utilisateurs` WHERE login = :login");
 		        $count->execute(array(':login' => htmlentities($_POST['login'])));
 		        $num_rows = $count->fetch(PDO::FETCH_ASSOC);
-		        // echo $num_rows;
-		        var_dump($num_rows);
 
-		         if (!$num_rows)
+		        if (!$num_rows)
 		        {
 		        	$rgt = "INSERT INTO utilisateurs (login, password) VALUES (:login, :password)";
-                
-                // DEBUG
-                echo "<pre>\n" . $rgt . "</pre>";
-    
-                // sanitizing input query
-                $stmt = $pdo->prepare($rgt);
-    
-                $stmt->execute([
-                    ':login' => htmlentities($_POST['login']), 
-                    ':password' => password_hash( htmlentities( $_POST['password']), PASSWORD_DEFAULT)
-                ]);
-
-                $ok =  'Votre profil a été créé avec succès!';
+                	$stmt = $pdo->prepare($rgt);
+                	$stmt->execute([
+                    				':login'    => htmlentities($_POST['login']), 
+                    				':password' => password_hash( htmlentities( $_POST['password']), PASSWORD_DEFAULT)
+                	]);
+                	$_SESSION['success'] =  'Votre profil a été créé avec succès!';
+                	header('location: inscription.php');
+                	return;
 
 		        }
 		        else 
 		        {
-		           $error = "Le login exite déja";
+		            $_SESSION['error'] = "Le login exite déja";
+		            header('location: inscription.php');
+                	return;
 		        }		
 			}
 			else
 			{
-				$error = "Tout doit faire au max 255 caractères";
+				$_SESSION['error'] = "Tout doit faire au max 255 caractères";
+				header('location: inscription.php');
+               	return;
 			}
 		}
 		else 
 		{
-			$error = "le mot de passe de confirmation dois correspondre au mot de passe";
+			$_SESSION['error'] = "le mot de passe de confirmation dois correspondre au mot de passe";
+			header('location: inscription.php');
+            return;
 		}
-	}
-
-
-
-
+}
 
 ?>
-            <form action="inscription.php" method="POST">
+	<!DOCTYPE html>
+	<html lang="fr">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+		<title>Inscription</title>
+	</head>
+	<body>
+		<main class="container">
+			<form action="inscription.php" method="POST">
 
-                <label for="login">Login:</label>
-                <input type="text" name="login" id="login" required /><br />
+	            <label for="login">Login:</label>
+	            <input type="text" name="login" id="login" required /><br />
 
-                <label for="password">Mot de passe:</label>
-                <input type="password" name="password" id="password" required /><br />
+	            <label for="password">Mot de passe:</label>
+	            <input type="password" name="password" id="password" required /><br />
 
-                <label for="passwordConfirm">Confirmation du mot de passe:</label>
-                <input type="password" name="password_confirm" required /><br />
+	            <label for="passwordConfirm">Confirmation du mot de passe:</label>
+	            <input type="password" name="password_confirm" required /><br />
 
-                <input type="submit" id="submitButton" name="submit" value="M'inscrire" />
-                <input type='submit' name='cancel' value='annuler' />          
-            </form>
-            <?php  
+	            <input type="submit" id="submitButton" name="submit" value="M'inscrire" />
+	            <input type='submit' name='cancel' value='annuler' />          
+	        </form>
+	        <?php  
 
-            	if (isset($error)) {
-            		echo $error;
-            		# code...
-            	}
-            	else echo $ok;
+	        	if (isset($_SESSION['error'])) 
+	        	{
+	        		echo '<p style = "color : red">'.  $_SESSION['error'] . "</p>";
+	        		unset($_SESSION['error']);
+	        	}
+	        	elseif (isset($_SESSION['success'])) 
+	        	{
+	        		echo '<p style = "color:green">'.  $_SESSION['success'] . "</p>";
+	        		unset($_SESSION['success']);
+	        	}
 
-            ?>
-
+	        ?>
+    	</main>
+	
+	</body>
+	</html>
             
