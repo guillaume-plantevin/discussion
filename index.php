a7918cd14d64ce1f0183e7b1bb974481b6bdb1ee
albertini <keany.albertini@laplateforme.io>
2020-12-30 19:08:02 +0100

    ajout du dossier font + police, dossier image, dossier template -> head + header

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<!-- Une page d’accueil qui présente votre site (index.php) -->
+<!DOCTYPE html>
+<html lang="fr">
 <?php
-  // Une page d’accueil qui présente votre site (index.php)
-
-
+$title = "accueil";
+require_once("templates/head.php")
 ?>
+
+<body class="body-accueil">
+
+	<?php 
+	require_once("templates/header.php")
+	?>
+	
+</body>
+</html>
--- inscription.php
+++ inscription.php
@@ -11,6 +11,8 @@
     session_start();
     require_once('pdo.php');
 
+    $title = "inscription";
+
 	if (isset($_POST['submit'])) 
 	{
 		$length_login = strlen($_POST['login']);
@@ -62,14 +64,10 @@
 ?>
 	<!DOCTYPE html>
 	<html lang="fr">
-	<head>
-		
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta charset="utf-8">
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="styles/style.css">
-		<title>Inscription</title>
-	</head>
+	<?php
+
+		require_once("templates/head.php")  
+	?>
 	<body>
 		<main class="container">
 			<form action="inscription.php" method="POST">
