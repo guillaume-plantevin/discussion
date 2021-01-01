2c85f32965c477f61ee610e3029ccfa12b094260
albertini <keany.albertini@laplateforme.io>
2021-01-01 20:01:34 +0100

    erreur avec le css

--- index.php
+++ index.php
@@ -12,5 +12,6 @@ require_once("templates/head.php")
 	require_once("templates/header.php")
 	?>
 	
+
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -2,4 +2,7 @@
 	<div class="div1-accueil">
 		<h1 class="h1-accueil"> FOOT-PROJECT </h1>
 	</div>
+	<P>DNFKNZEKS DJSI </P>
+
+
 </header>
