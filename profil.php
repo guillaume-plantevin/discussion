92866ec213ca1c8b27cc74fd802d3f70c3309ad4
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-01-01 23:34:21 +0100

    avance sur discussion: fetch les messages, presentation en tableau pas top mais c'est un debut

--- profil.php
+++ profil.php
@@ -3,4 +3,25 @@
     Une page permettant de modifier son profil (profil.php) :
     Cette page possède un formulaire permettant à l’utilisateur de modifier son login et son mot de passe.
   */
+  session_start();
+
+  require_once('pdo.php');
+  require_once('functions/functions.php');
+
+  $title = "profil";
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <?php
+    require_once("templates/head.php");
+  ?>
+  <body>
+  <main>
+    <h1>Page de Profil</h1>
+
+  
+  
+  </main>
+    <?php require_once('templates/footer.php');?>
+  </body>
+</html>
