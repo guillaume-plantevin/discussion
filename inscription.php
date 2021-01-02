1d59ab99a23f0b7713e1b020b7f061d8e1f7ae50
guillaume-plantevin <71876345+guillaume-plantevin@users.noreply.github.com>
2021-01-02 10:43:18 +0100

    Update inscription.php
    suppresion du $_SESSION['success'] dans le VIEW, car redirection vers connexion.php

--- inscription.php
+++ inscription.php
@@ -107,10 +107,12 @@
 	        		echo '<p class="error" >'.  $_SESSION['error'] . "</p>";
 	        		unset($_SESSION['error']);
 	        	}
+			/*
 	        	elseif (isset($_SESSION['success'])) {
 	        		echo '<p class="success" >'.  $_SESSION['success'] . "</p>";
 	        		unset($_SESSION['success']);
 	        	}
+			*/
 	        ?>
 			<form action="inscription.php" method="POST">
 	            <label for="login">Login:</label>
