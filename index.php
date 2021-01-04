c87aa38c91e8cecf55d6343b6632fe64805eacfa
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-01-04 10:53:34 +0100

    correction bugs d'affichage

--- index.php
+++ index.php
@@ -9,14 +9,15 @@
 	<body class="body-accueil">
 		<?php require_once("templates/header.php") ?>
 		<main class="'container">
-			<h1>Accueil</h1>
-      <article class="article-accueil presentation">
-      <!-- changement du h1 initial en h2 -->
-		  <h2>Venez rencontrer des passionnés comme vous !</h2>			
-			<p>Ici, vous pourrez <strong> partager, échanger, discuter</strong> avec des milliers de personnes à travers le monde.<br> 
-        Pour cela, il vous suffilt de vous <strong>enregistrer</strong> puis de vous <strong>connecter</strong> afin d'accéder à notre fil de discution en <strong>illimité</strong>. 
-      </p>
-		</article>
+			<!-- <article class="article-accueil presentation"> -->
+			<article class="article-accueil presentation">
+				<h1>Accueil</h1>
+			<!-- changement du h1 initial en h2 -->
+				<h2>Venez rencontrer des passionnés comme vous !</h2>			
+				<p>Ici, vous pourrez <strong> partager, échanger, discuter</strong> avec des milliers de personnes à travers le monde.<br> 
+				Pour cela, il vous suffit de vous <strong>enregistrer</strong> puis de vous <strong>connecter</strong> afin d'accéder à notre fil de discution. 
+			</p>
+				</article>
 		</main>
 		<?php require_once('templates/footer.php'); ?>
 	</body>
